281ed27dde861686cadf9f21f0f9fe71d95f6e50
Parthiban P <parthibanprabha.info@gmail.com>
2026-07-14 23:19:32 +0530

    tested log file

--- test_log.py
+++ test_log.py
@@ -7,4 +7,4 @@ spark = (
 log_df = spark.read.text("data/access.log")
 
 log_df.show(truncate=False)
-print("Total number of log entries:", log_df.count())
+print("Total number of log entrie:", log_df.count())
