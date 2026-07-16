0a211f3631345b364579f2d88ee27116678b0c95
Parthiban P <parthibanprabha.info@gmail.com>
2026-07-16 22:39:39 +0530

    Testing pyspark

--- test_log.py
+++ test_log.py
@@ -6,5 +6,5 @@ spark = (
 
 log_df = spark.read.text("data/access.log")
 
-log_df.show(truncate=False)
-print("Total number of log entrie:", log_df.count())
+print(log_df.show())
+print(log_df.printSchema())
---
+++ testing_pyspark.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession 
+
+spark = (
+    SparkSession.builder.appName("Learning Pyspark").getOrCreate()
+)
+
+log_df = spark.read.text("data/access.log")
+
+# print("Schema:")
+# log_df.printSchema()
+
+# print("Columns:")
+# print(log_df.columns)
+
+print("First 5 rows")
+log_df.show(5, truncate=False)
+
+# print("Total Experiments")
+# print(log_df.count())
