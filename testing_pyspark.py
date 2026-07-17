2c063a61872fdab8ce89aad6eaea696c6055fa6d
Parthiban P <parthibanprabha.info@gmail.com>
2026-07-17 22:30:50 +0530

    Experimented pyspark

--- testing_pyspark.py
+++ testing_pyspark.py
@@ -6,14 +6,16 @@ spark = (
 
 log_df = spark.read.text("data/access.log")
 
-# print("Schema:")
-# log_df.printSchema()
+print("Schema:")
+log_df.printSchema()
 
-# print("Columns:")
-# print(log_df.columns)
+print("Columns:")
+print(log_df.columns)
 
 print("First 5 rows")
 log_df.show(5, truncate=False)
 
-# print("Total Experiments")
-# print(log_df.count())
+print("Total Experiments")
+print(log_df.count())
+
+print("no of columns : ", len(log_df.columns))
